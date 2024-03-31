e059669b6ac9d43f2a8eb8005778525dd4eecd5d
VickyA5 <vavalos@fi.uba.ar>
2024-03-31 00:03:39 -0300

    avanzo con el servidor

--- servidor.cpp
+++ servidor.cpp
@@ -11,34 +11,94 @@ int Servidor::establecer_conexion() {
     return 0;
 }
 
-std::string Servidor::recibir_acciones() {
-    std::vector<unsigned char> datos_recibidos;
+std::vector<uint16_t> Servidor::recibir_acciones() {
+    unsigned int tamano_buffer = 1024;
+    std::vector<uint8_t> buffer(tamano_buffer);
     bool conexion_cerrada = false;
-    std::string acciones_realizadas;
-
-    while (!seRecibioNOP(datos_recibidos)) {
-        int bytes_recibidos = socket.recvall(datos_recibidos, &conexion_cerrada);
-
-        if (conexion_cerrada || bytes_recibidos == -1) {
-            if (conexion_cerrada) {
-                std::cerr << "Conexión cerrada por el cliente." << std::endl;
-            } else {
-                std::cerr << "Error al recibir datos del cliente." << std::endl;
-            }
-            break;
-        }
-
-        if (bytes_recibidos > 0) {
-            std::string acciones = interpretarAcciones(datos_recibidos, bytes_recibidos);
-            acciones_realizadas += acciones;
+    int bytes_recibidos = aceptador.recvall(buffer.data(), tamano_buffer, &conexion_cerrada);
+    //Ya puse en el buffer las acciones serializadas.
+    if (conexion_cerrada || bytes_recibidos == -1) {
+        if (conexion_cerrada) {
+            std::cerr << "Conexión cerrada por el cliente." << std::endl;
+        } else {
+            std::cerr << "Error al recibir datos del cliente." << std::endl;
         }
     }
+    std::vector<uint16_t> acciones_realizadas = interpretar_acciones(buffer);
 
     return acciones_realizadas;
 }
 
-bool seRecibioNOP(const std::vector<unsigned char>& datos) {
-    // Verificar si se recibió la instrucción NOP
-    std::string accion_actual = interpretarAcciones(datos, datos.size());
-    return (accion_actual.find("NOP") != std::string::npos);
+
+//Interpreta una sola serie de acciones o todo?
+std::vector<uint16_t> Servidor::interpretar_acciones(const std::vector<uint8_t>& buffer) {
+    std::vector<uint16_t> acciones_interpretadas;
+    size_t i = 0;
+    while (i < buffer.size()) {
+        uint8_t accion_actual = buffer[i];
+        switch (accion_actual) {
+            case 0x00: // NOP
+                avanzar_buffer(i, 1);
+                break;
+            case 0x01: // JUMP
+                interpretar_jump(buffer, i, acciones_interpretadas);
+                break;
+            case 0x03: // LEFT
+                interpretar_left(buffer, i, acciones_interpretadas);
+                break;
+            case 0x04: // DUCK
+                agregar_accion(DUCK, acciones_interpretadas);
+                avanzar_buffer(i, 1);
+                break;
+            case 0x05: // HIT
+                interpretar_hit(buffer, i, acciones_interpretadas);
+                break;
+            default:
+                avanzar_buffer(i, 1); //NO DEBERÍA PASAR
+                break;
+        }
+    }
+    //acciones_interpretadas.push_front(); EL TAMAÑO SIN CONTAR NOP
+    return acciones_interpretadas;
+}
+
+void Servidor::avanzar_buffer(size_t& index, size_t cantidad) {
+    index += cantidad;
+}
+//HACER QUE DEVUELVAN COMO LO PIDE EL ENUNCIADO
+void Servidor::interpretar_jump(const std::vector<uint8_t>& buffer, size_t& index, std::vector<uint16_t>& acciones_interpretadas) {
+    if (index + 2 < buffer.size() && buffer[index + 1] == 0x01 && buffer[index + 2] == 0x05) {
+        agregar_accion(UPPERCUT, acciones_interpretadas);
+        avanzar_buffer(index, 3);
+    } else {
+        agregar_accion(JUMP, acciones_interpretadas);
+        avanzar_buffer(index, 1);
+    }
+}
+
+void Servidor::interpretar_left(const std::vector<uint8_t>& buffer, size_t& index, std::vector<uint16_t>& acciones_interpretadas) {
+    if (index + 3 < buffer.size() && buffer[index + 1] == 0x02 && buffer[index + 2] == 0x01 && buffer[index + 3] == 0x05) {
+        agregar_accion(HIGHKICK, acciones_interpretadas);
+        avanzar_buffer(index, 4);
+    } else {
+        agregar_accion(LEFT, acciones_interpretadas);
+        avanzar_buffer(index, 1);
+    }
+}
+
+void Servidor::interpretar_hit(const std::vector<uint8_t>& buffer, size_t& index, std::vector<uint16_t>& acciones_interpretadas) {
+    if (index + 2 < buffer.size() && buffer[index + 1] == 0x04 && buffer[index + 2] == 0x03) {
+        agregar_accion(SIDEKICK, acciones_interpretadas);
+        avanzar_buffer(index, 3);
+    } else {
+        agregar_accion(HIT, acciones_interpretadas);
+        avanzar_buffer(index, 1);
+    }
+}
+
+void Servidor::agregar_accion(const std::string& accion, std::vector<uint16_t>& datos) {
+    for (char c : accion) {
+        uint16_t ascii_hex = static_cast<uint16_t>(c);
+        datos.push_back(ascii_hex);
+    }
 }
--- servidor.h
+++ servidor.h
@@ -5,21 +5,35 @@
 #include <iostream>
 #include <vector>
 
+#define JUMP "JUMP"
+#define UPPERCUT "UPPERCUT"
+#define DUCK "DUCK"
+#define HIT "HIT"
+#define LEFT "LEFT"
+#define RIGHT "RIGHT"
+#define SIDEKICK "SIDEKICK"
+#define HIGHKICK "HIGHKICK"
+
 class Servidor {
 
 private:
     Socket aceptador;
     //Devuelve true si se recibió la instrucción NOP
     bool seRecibioNOP(const std::vector<unsigned char>& datos);
-
+    void avanzar_buffer(size_t& index, size_t cantidad);
+    void interpretar_jump(const std::vector<uint8_t>& buffer, size_t& index, std::vector<uint16_t>& acciones_interpretadas);
+    void interpretar_left(const std::vector<uint8_t>& buffer, size_t& index, std::vector<uint16_t>& acciones_interpretadas);
+    void interpretar_hit(const std::vector<uint8_t>& buffer, size_t& index, std::vector<uint16_t>& acciones_interpretadas);
+    void agregar_accion(const std::string& accion, std::vector<uint16_t>& datos);
 public:
     Servidor(const char *nombre_aceptador);
     //Dada la serie de acciones, identifica los combos y devuelve la serie de acciones
     //con los combos correspondientes.
-    std::string identificar_combo();
+    std::vector<uint16_t> interpretar_acciones(const std::vector<uint8_t>& buffer);
     //EL socket aceptador acepta la comunicación con el cliente y se delega ...
     int establecer_conexion();
-    std::string recibir_acciones();
+    //Recibe las acciones serializadas del cliente y delega su interpretación.
+    std::vector<uint16_t> recibir_acciones();
 };
 
 
